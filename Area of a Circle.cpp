801b36d49ca8d6d9b82fbe54ce44516495bf76ac
Maha6468 <motherma7633@gmail.com>
2026-05-05 00:12:27 +0600

    update

---
+++ Area of a Circle.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double R, A;
+
+    cin >> R;
+
+    A = 3.14159 * R * R;
+
+    cout << fixed << setprecision(2);
+    cout << "A=" << A << endl;
+
+    return 0;
+}
